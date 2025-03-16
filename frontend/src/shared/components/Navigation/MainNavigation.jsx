5be59ef91f865a1be9dd5eb8bcd2df638a00ae6b
EQKNF <emil.fredriksen@hotmail.no>
2025-03-16 18:48:01 +0100

    feat: add backdrop component and integrate with side drawer; update styles and animations

--- frontend/src/shared/components/Navigation/MainNavigation.jsx
+++ frontend/src/shared/components/Navigation/MainNavigation.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import SideDrawer from "./SideDrawer";
 import NavLinks from "./NavLinks";
+import Backdrop from "../UIElements/Backdrop";
 
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => setDrawerIsOpen(true);
+  const closeDrawer = () => setDrawerIsOpen(false);
+
   return (
     <>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen ? (
+        <>
+          <SideDrawer>
+            <nav className="main-navigation__drawer-nav">
+              <NavLinks />
+            </nav>
+          </SideDrawer>
+          <Backdrop onClick={closeDrawer} />
+        </>
+      ) : null}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
