440e448b97f155c4d8a593930ec657a6960aeed3
EQKNF <emil.fredriksen@hotmail.no>
2025-03-17 22:01:42 +0100

    feat: implement form handling in NewPlace component; add input validation and styling

--- frontend/src/places/pages/UserPlaces.jsx
+++ frontend/src/places/pages/UserPlaces.jsx
@@ -19,14 +19,14 @@ const DUMMY_PLACES = [
   },
   {
     id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    title: "Aker brygge",
+    description: "The capital city of Norway.",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg",
-    address: "20 W 34th St, New York, NY 10118, United States",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/14-09-02-oslo-RalfR-236.jpg/1920px-14-09-02-oslo-RalfR-236.jpg",
+    address: "Oslo, Norway",
     location: {
-      lat: 40.7484405,
-      lng: -73.9878531,
+      lat: 59.9139,
+      lng: 10.7522,
     },
     creator: "u2",
   },
